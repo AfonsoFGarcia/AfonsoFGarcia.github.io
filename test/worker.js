21576f414a755d5de3d4d11332d96cd36dfd476f
Afonso F. Garcia <afonsogarcia@icloud.com>
2018-06-06 16:32:39 +0200

    Update worker.js

--- test/worker.js
+++ test/worker.js
@@ -1,6 +1,20 @@
 onmessage = function(e) {
-  console.log('Message received from main script');
-  var workerResult = 'Result: ' + (e.data[0] * e.data[1]);
-  console.log('Posting message back to main script');
-  postMessage(workerResult);
+  function* prime(currentInteger = 2, finalInteger = null) {
+    function isPrime(integerToTest) {
+      for (let i = 2; i < integerToTest; i++) {
+        if (integerToTest%i === 0) {
+          return false;
+        }
+      }
+      return true;
+    }  
+    while (finalInteger === null || finalInteger > currentInteger) {
+      if (isPrime(currentInteger++)) {
+        yield currentInteger-1;
+      }
+    }
+  }
+  
+  for(num of prime(e.data[0], e.data[1]))
+    postMessage(num);
 }
